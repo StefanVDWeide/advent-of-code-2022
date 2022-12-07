04ddbbcb585d55e63754595ddbf5e865f791d37f
stefanvdWeide <stefan.vander.weide@hotmail.com>
2022-12-07 12:07:46 +0100

    Solved day 5

---
+++ src/day_6/mod.rs
@@ -0,0 +1,82 @@
+use std::collections::HashSet;
+use std::fs;
+
+pub fn solution() {
+    // Read the input.txt file provided by the Advent of Code website
+    let input = fs::read_to_string("src/day_6/input.txt").expect("Error during reading the input");
+
+    // Trim the end of the file to ensure no \n will remain to screw you over later
+    let trimmed_input = input.trim_end();
+
+    let mut initial_marker_start_of_packet: Vec<&str> =
+        trimmed_input[..4].split_terminator("").skip(1).collect();
+
+    let initial_unique_set: HashSet<&str> =
+        initial_marker_start_of_packet.clone().into_iter().collect();
+
+    if initial_unique_set.len() == 4 {
+        println!(
+            "The inital 4 characters form a unique marker: {:?}",
+            initial_unique_set
+        );
+    }
+
+    let individual_characters: Vec<&str> = trimmed_input.split_terminator("").skip(5).collect();
+
+    for index in 0..individual_characters.len() {
+        initial_marker_start_of_packet.remove(0);
+
+        initial_marker_start_of_packet.push(individual_characters[index]);
+
+        let loop_unique_set: HashSet<&str> =
+            initial_marker_start_of_packet.clone().into_iter().collect();
+
+        if loop_unique_set.len() == 4 {
+            println!(
+                "These 4 characters form a unique marker: {:?} after {} characters",
+                loop_unique_set,
+                index + 5
+            );
+            return;
+        }
+    }
+    // Part 2
+    // ####################################################################################
+    let mut initial_marker_start_of_message: Vec<&str> =
+        trimmed_input[..14].split_terminator("").skip(1).collect();
+
+    let initial_unique_set_messages: HashSet<&str> = initial_marker_start_of_message
+        .clone()
+        .into_iter()
+        .collect();
+
+    if initial_unique_set_messages.len() == 14 {
+        println!(
+            "The inital 14 characters form a unique marker: {:?}",
+            initial_unique_set
+        );
+    }
+
+    let individual_characters_start_of_message: Vec<&str> =
+        trimmed_input.split_terminator("").skip(15).collect();
+
+    for index in 0..individual_characters_start_of_message.len() {
+        initial_marker_start_of_message.remove(0);
+
+        initial_marker_start_of_message.push(individual_characters_start_of_message[index]);
+
+        let loop_unique_set: HashSet<&str> = initial_marker_start_of_message
+            .clone()
+            .into_iter()
+            .collect();
+
+        if loop_unique_set.len() == 14 {
+            println!(
+                "These 14 characters form a unique marker: {:?} after {} characters",
+                loop_unique_set,
+                index + 15
+            );
+            return;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod day_2;
 mod day_3;
 mod day_4;
 mod day_5;
+mod day_6;
 
 // Main entry point for the program
 fn main() {
@@ -10,5 +11,6 @@ fn main() {
     // day_2::solution();
     // day_3::solution();
     // day_4::solution();
-    day_5::solution();
+    // day_5::solution();
+    day_6::solution();
 }
