fc097569792bfc9c1f65376f780483921d396ff2
stefanvdWeide <stefan.vander.weide@hotmail.com>
2022-12-08 16:15:50 +0100

    Solved day 7

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod day_3;
 mod day_4;
 mod day_5;
 mod day_6;
+mod day_7;
 
 // Main entry point for the program
 fn main() {
@@ -12,5 +13,6 @@ fn main() {
     // day_3::solution();
     // day_4::solution();
     // day_5::solution();
-    day_6::solution();
+    // day_6::solution();
+    day_7::solution();
 }
